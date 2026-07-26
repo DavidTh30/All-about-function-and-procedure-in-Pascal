51d2249e4627216e37a7bd2e125363627448aed5
Neramit <DavidTh30@gmail.com>
2026-07-26 10:12:11 +0700

    Add files via upload

---
+++ 01/unit1.pas
@@ -0,0 +1,167 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  unit2;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    PaintBox1: TPaintBox;
+    procedure Button1Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+  private
+
+  public
+    constructor Create(TheOwner: TComponent); override;
+    procedure OnIdle(Sender: TObject; var Done: boolean);
+    procedure OnIdleEnd(Sender: TObject);
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  MessageDlg('Hello');
+  MessBox('Hello');
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  FBuffer.Free;
+end;
+
+{ TForm1 }
+constructor TForm1.Create(TheOwner: TComponent);
+begin
+  inherited Create(TheOwner);
+
+  FBuffer := TBitmap.Create;
+  FBuffer.SetSize(PaintBox1.Width, PaintBox1.Height);
+  //FBuffer.Canvas.Brush.Color := clSilver;
+  FBuffer.Canvas.Brush.Color := $00C8D0D4;
+  FBuffer.Canvas.FillRect(0, 0, FBuffer.Width, FBuffer.Height);
+
+  theta:=0;
+
+  Application.OnIdle := @OnIdle;
+  Application.OnIdleEnd:=@OnIdleEnd;
+
+end;
+
+procedure TForm1.OnIdle(Sender: TObject; var Done: boolean);
+var
+  Origin: TPoint = (X:150; y:150);
+  P1: TPoint = (X: 0; y:0);
+  P2: TPoint = (X: 60; y:0);
+  P3: TPoint= (X:60; y:60);
+  IsWindows:boolean = false;
+  IsCompiler32:boolean = false;
+  IsCompiler64:boolean = false;
+  x:integer;
+  y:integer;
+begin
+  {$IFDEF WIN32}
+    ElapsedTime32 := GetTickCount-StartTime32;
+    StartTime32 := GetTickCount;
+  {$ENDIF}
+
+  {$IFDEF WIN64}
+    ElapsedTime64 := GetTickCount64 - StartTime64;
+    StartTime64 := GetTickCount64;
+  {$ENDIF}
+
+  {$IFDEF MSWINDOWS}
+    IsWindows:=true;
+  {$ENDIF}
+
+  if SizeOf(Pointer) = 4 then IsCompiler32:=true; // 32-bit environment.
+  if SizeOf(Pointer) = 8 then IsCompiler64:=true; // 64-bit environment.
+
+  FBuffer.Canvas.Clear;
+
+  {$IFDEF WIN32}
+    FBuffer.Canvas.TextOut(0,0,'ElapsedTime32: '+ElapsedTime32.ToString);
+  {$ENDIF}
+
+  {$IFDEF WIN64}
+    FBuffer.Canvas.TextOut(0,20,'ElapsedTime64: '+ElapsedTime64.ToString);
+  {$ENDIF}
+
+  MyData:=Message01('Test');
+  FBuffer.Canvas.TextOut(0,40,'Message01: '+MyData.Str_);
+  Message02(MyData);
+  FBuffer.Canvas.TextOut(0,60,'Message02: '+MyData.Str_);
+  Message03(MyData);
+  FBuffer.Canvas.TextOut(0,80,'Message03: '+MyData.Str_);
+  MyData:=Message04();
+  FBuffer.Canvas.TextOut(0,100,'Message04: '+MyData.Str_);
+  Message05(MyData);
+  FBuffer.Canvas.TextOut(0,120,'Message05: '+MyData.Str_);
+  MyData:=Message05(MyData);
+  FBuffer.Canvas.TextOut(0,140,'Message05: '+MyData.Str_);
+  MyData.Str_:=Format__('Str: %s, Int: %d, Float: %f', ['Test', 456, 12.4]);
+  FBuffer.Canvas.TextOut(0,160,'Format: '+MyData.Str_);
+  MyData.Str_:=SumAll ([10 * 10, 'k', True, 10.34, '99999']).ToString;
+  FBuffer.Canvas.TextOut(0,180,'SumAll: '+MyData.Str_);
+  MyData.Str_:=min_(3.141597,Byte(4)).ToString;
+  FBuffer.Canvas.TextOut(0,200,'min: '+MyData.Str_);
+  MyData.Str_:=ShowMsg(123,'ABC');
+  FBuffer.Canvas.TextOut(0,220,'ShowMsg: '+MyData.Str_);
+  FBuffer.Canvas.TextOut(0,240,'IsWindows: '+IsWindows.ToInteger.ToString);
+  FBuffer.Canvas.TextOut(0,260,'Compiler 32bit: '+IsCompiler32.ToInteger.ToString);
+  FBuffer.Canvas.TextOut(0,280,'Compiler 64bit: '+IsCompiler64.ToInteger.ToString);
+  FBuffer.Canvas.TextOut(0,300,'Windows 64bit: '+IsWindows64.ToInteger.ToString);
+
+  FBuffer.Canvas.Pen.Width:=3;
+  FBuffer.Canvas.Pen.Color:=clBlue;
+  //FBuffer.Canvas.MoveTo(Origin);
+
+  x:= Round((P1.x*cos(theta)) - (P1.y*sin(theta)));
+  y:= Round((P1.x*sin(theta)) + (P1.y*cos(theta)));
+  P1.X:=x; P1.Y:=y;
+  P1:=P1+Origin;
+  FBuffer.Canvas.MoveTo(P1);
+
+  x:= Round((P2.x*cos(theta)) - (P2.y*sin(theta)));
+  y:= Round((P2.x*sin(theta)) + (P2.y*cos(theta)));
+  P2.X:=x; P2.Y:=y;
+  P2:=P2+Origin;
+  FBuffer.Canvas.LineTo(P2);
+
+  x:= Round((P3.x*cos(theta)) - (P3.y*sin(theta)));
+  y:= Round((P3.x*sin(theta)) + (P3.y*cos(theta)));
+  P3.X:=x; P3.Y:=y;
+  P3:=P3+Origin;
+  FBuffer.Canvas.LineTo(P3);
+
+  FBuffer.Canvas.LineTo(P1);
+
+  PaintBox1.Canvas.Draw(0, 0, FBuffer);
+
+  //Application.OnIdle := nil;
+
+
+  theta:=theta+0.002;
+  Done := false;
+end;
+
+procedure TForm1.OnIdleEnd(Sender: TObject);
+begin
+  // OnIdleEnd run when OnIdle  Done := true;
+end;
+
+end.
+
---
+++ 01/unit2.pas
@@ -0,0 +1,272 @@
+unit Unit2;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, windows, Forms, Controls, Graphics, Dialogs, StdCtrls, StrUtils,
+  ComCtrls;
+
+type
+  TVarRec = record
+    case Byte of
+      vtInteger:    (VInteger: Integer; VType: Byte);
+      vtBoolean:    (VBoolean: Boolean);
+      vtChar:       (VChar: Char);
+      vtExtended:   (VExtended: PExtended);
+      vtString:     (VString: PShortString);
+      vtPointer:    (VPointer: Pointer);
+      vtPChar:      (VPChar: PChar);
+      vtObject:     (VObject: TObject);
+      vtClass:      (VClass: TClass);
+      vtWideChar:   (VWideChar: WideChar);
+      vtPWideChar:  (VPWideChar: PWideChar);
+      vtAnsiString: (VAnsiString: Pointer);
+      vtCurrency:   (VCurrency: PCurrency);
+      vtVariant:    (VVariant: PVariant);
+      vtInterface:  (VInterface: Pointer);
+  end;
+
+type
+  P_TclassData = ^TclassData;
+  TclassData = class
+    Name_: string;
+    Prefix: string;
+    Str_: string;
+    Int_: Integer;
+    Suffix: string;
+  end;
+
+type
+  P_TRecordData = ^TRecordData;
+  TRecordData = record
+    Name_: string;
+    Prefix: string;
+    Str_: string;
+    Int_: Integer;
+    Suffix: string;
+  end;
+
+procedure log2(LINENUM_: integer; message_: string);
+function Message01(message_: string): TRecordData;
+function Message02(out message_: TRecordData): TRecordData;
+function Message03(var message_: TRecordData): TRecordData;
+function Message04(const message_: string = 'Test'): TRecordData;
+function Message05(message_: TRecordData): TRecordData;
+function Format__(const Format_: string; const Args: array of const): string;
+function SumAll (const Args: array of const): Extended;
+
+function Min_(A,B: Integer): Integer; overload;
+function Min_(A,B: Int64): Int64; overload;
+function Min_(A,B: Single): Single; overload;
+function Min_(A,B: Double): Double; overload;
+function Min_(A,B: Extended): Extended; overload;
+
+function ShowMsg(str_: string): String; overload;
+function ShowMsg(FormatStr_: string; Params: array of const): String; overload;
+function ShowMsg(I: Integer; str_: string): String; overload;
+
+procedure MessageDlg (str: string); overload;
+
+procedure MessBox (Msg: string; Caption: string = 'Warning'; Flags: LongInt = mb_OK or mb_IconHand);
+
+function IsWindows64: boolean;
+
+type
+  Unit_ = class(TForm)
+  private
+
+  public
+    procedure Button2Click(Sender: TObject);
+  end;
+
+var
+  Memo_:TMemo;
+  MyData: TRecordData;
+  StartTime32, ElapsedTime32: LongInt;
+  StartTime64, ElapsedTime64: Cardinal;
+  FBuffer:TBitmap;
+  theta:extended;
+
+implementation
+
+procedure log2(LINENUM_: integer; message_: string);
+begin
+  Memo_.Append(LINENUM_.ToString+message_);
+end;
+
+procedure Unit_.Button2Click(Sender: TObject);
+begin
+
+end;
+
+function Message01(message_: string): TRecordData;
+begin
+
+  Result.Str_ := message_;
+
+end;
+
+function Message02(out message_: TRecordData): TRecordData;
+begin
+  message_.Str_:='#'+message_.Str_+'#';
+  Result:= message_;
+
+end;
+
+function Message03(var message_: TRecordData): TRecordData;
+begin
+  message_.Str_:='!'+message_.Str_+'!';
+  Result:= message_;
+
+end;
+
+function Message04(const message_: string = 'Test'): TRecordData;
+begin
+
+  Result.Str_ := message_;
+
+end;
+
+function Message05(message_: TRecordData): TRecordData;
+begin
+  message_.Str_:='??'+message_.Str_+'??';
+  Result:= message_;
+
+end;
+
+function Format__(const Format_: string; const Args: array of const): string;
+begin
+  Result:= Format(Format_, Args);
+end;
+
+function SumAll (const Args: array of const): Extended;
+var
+  I: Integer;
+begin
+  Result := 0;
+  for I := Low(Args) to High (Args) do
+    case Args [I].VType of
+      vtInteger: Result :=
+        Result + Args [I].VInteger;
+      vtBoolean:
+        if Args [I].VBoolean then
+          Result := Result + 1;
+      vtChar:
+        Result := Result + Ord (Args [I].VChar);
+      vtExtended:
+        Result := Result + Args [I].VExtended^;
+      vtString, vtAnsiString:
+        Result := Result + StrToIntDef ((Args [I].VString^), 0);
+      vtWideChar:
+        Result := Result + Ord (Args [I].VWideChar);
+      vtCurrency:
+        Result := Result + Args [I].VCurrency^;
+    end; // case
+end;
+
+function Min_(A,B: Integer): Integer; overload;
+begin
+  if A <= B then
+    Result:=A
+  else
+    Result:=b;
+end;
+
+function Min_(A,B: Int64): Int64; overload;
+begin
+  if A <= B then
+    Result:=A
+  else
+    Result:=b;
+end;
+
+function Min_(A,B: Single): Single; overload;
+begin
+  if A <= B then
+    Result:=A
+  else
+    Result:=b;
+end;
+
+function Min_(A,B: Double): Double; overload;
+begin
+  if A <= B then
+    Result:=A
+  else
+    Result:=b;
+end;
+
+function Min_(A,B: Extended): Extended; overload;
+begin
+  if A <= B then
+    Result:=A
+  else
+    Result:=b;
+end;
+
+function ShowMsg(str_: string): String; overload;
+begin
+  Result:=str_;
+end;
+
+function ShowMsg(FormatStr_: string; Params: array of const): String; overload;
+begin
+  Result:=Format(FormatStr_, Params);
+end;
+
+function ShowMsg(I: Integer; str_: string): String; overload;
+begin
+  Result:=IntToStr (I) + ' ' + str_;
+end;
+
+procedure MessageDlg(str: string); overload;
+begin
+  Dialogs.MessageDlg (str, mtInformation, [mbOK], 0);
+end;
+
+procedure MessBox (Msg: string; Caption: string = 'Warning'; Flags: LongInt = mb_OK or mb_IconHand);
+begin
+  Application.MessageBox (PChar (Msg),
+    PChar (Caption), Flags);
+end;
+
+function IsWindows64: boolean;
+{
+Detect if we are running on 64 bit Windows or 32 bit Windows,
+independently of bitness of this program.
+Original source:
+http://www.delphipraxis.net/118485-ermitteln-ob-32-bit-oder-64-bit-betriebssystem.html
+modified for FreePascal in German Lazarus forum:
+http://www.lazarusforum.de/viewtopic.php?f=55&t=5287
+}
+{$ifdef WIN32} //Modified KpjComp for 64bit compile mode
+type
+  TIsWow64Process = function( // Type of IsWow64Process API fn
+      Handle: Windows.THandle; var Res: Windows.BOOL): Windows.BOOL; stdcall;
+var
+  IsWow64Result: Windows.BOOL; // Result from IsWow64Process
+  IsWow64Process: TIsWow64Process; // IsWow64Process fn reference
+begin
+  // Try to load required function from kernel32
+  IsWow64Process := TIsWow64Process(Windows.GetProcAddress(
+    Windows.GetModuleHandle('kernel32'), 'IsWow64Process'));
+  if Assigned(IsWow64Process) then
+  begin
+    // Function is implemented: call it
+    if not IsWow64Process(Windows.GetCurrentProcess, IsWow64Result) then
+      raise SysUtils.Exception.Create('IsWindows64: bad process handle');
+    // Return result of function
+    Result := IsWow64Result;
+  end
+  else
+    // Function not implemented: can't be running on Wow64
+    Result := False;
+{$else} //if were running 64bit code, OS must be 64bit :)
+begin
+ Result := True;
+{$endif}
+end;
+
+end.
